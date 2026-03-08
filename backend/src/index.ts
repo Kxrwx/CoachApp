7d30f50831bfa6638d040d78de43d970b3039539
Kxrwx <maxime.iffland@icloud.com>
2026-03-08 20:56:32 +0100

    Init du projet

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend Ascend Node.js fonctionne ✅");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
