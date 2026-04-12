0107800a66b67a2b57527c5f451642c007feaf8a
Kxrwx <maxime.iffland@icloud.com>
2026-04-12 12:20:30 +0200

    Reset

--- backend/src/models/training/getData.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "../../utils/prisma";
-
-export async function GetAllTraining(userId:string) {
-    const req = await prisma.plannedWorkout.findMany({
-        where : {userId}
-    })
-    return req
-}
